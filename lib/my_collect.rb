b531387ce5e8e18beae376244d6865cad1da9c48
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-02-21 18:25:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+ def my_each(array)
+   empty_array = []
+  i = 0
+  while i < array.length
+# The "while" method will always return a value of => nil"
+    empty_array << yield(array[i])
+    i = i + 1
+  end
+end
 
